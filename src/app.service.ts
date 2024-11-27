1956a38810bd52158189a2327783325ab7bc1304
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-11-28 01:09:23 +0200

    👤 chore: exploring nest architecture

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  helloNest(): string {
+    return 'Nest says hello';
   }
 }
