4820da6ddfb7a24a30f019c8be2257e3953fff5a
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-11-29 23:03:06 +0200

    👤 feat: adding pipes and DTO

---
+++ src/users/dto/signup-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsInt, IsNotEmpty, IsString, Min, MinLength } from "class-validator";
+
+export class signUpDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength( 3)
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  @MinLength(8)
+  email: string;
+
+  @IsInt()
+  @Min(10)
+  age: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
-import {  Controller, Post, Req, Res } from "@nestjs/common";
+import {  Body, Controller, Post, Req, Res } from "@nestjs/common";
 import { userService } from "./users.service";
 import { Request, Response } from "express";
+import { signUpDto } from "./dto/signup-user.dto";
 
 @Controller('users')
 export class UserController {
   constructor(private readonly appService: userService) {}
   @Post('signup')
   signUpHandler(
+    @Body() body: signUpDto,
     @Req() req: Request,
     @Res() res: Response
   ) {
-    console.log(req.body);
+    console.log(body);
     console.log(req.query);
     const response = this.appService.signup();
 
