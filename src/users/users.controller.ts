aee259110ff3fecff90d6da1587ca82591546997
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-11-29 17:28:40 +0200

    ?? feat: sending data to server

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from "@nestjs/common";
+import {  Controller, Post, Req, Res } from "@nestjs/common";
 import { userService } from "./users.service";
+import { Request, Response } from "express";
 
 @Controller('users')
 export class UserController {
   constructor(private readonly appService: userService) {}
-  @Get('signup')
-  signUp(): string {
-    return this.appService.signup();
+  @Post('signup')
+  signUpHandler(
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    console.log(req.body);
+    console.log(req.query);
+    const response = this.appService.signup();
+
+    return res.json({ message : response });
   }
 }
